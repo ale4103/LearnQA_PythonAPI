143d83d677681968dce18a32cd54bf4833ae6abb
alexander.ivanov <ale4103@gmail.com>
2022-01-26 16:24:14 +0300

    GET /api/get_text

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import requests
+
+response = requests.get("https://playground.learnqa.ru/api/hello")
+print(response.text)
+
+response = requests.get("https://playground.learnqa.ru/api/get_text")
+print(response.text)
+
 print("Hello from Alexander!")
