e634e9334e872daebd47a47409be90e01e7a44e3
alexander.ivanov <ale4103@gmail.com>
2022-02-09 21:51:14 +0300

    homework

---
+++ ex12_header.py
@@ -0,0 +1,12 @@
+import requests
+
+class TestFirstAPI:
+    def test_hello_header(self):
+        url = "https://playground.learnqa.ru/api/homework_header"
+        expected_val = 'Some secret value'
+
+        response = requests.post(url)
+        actual_val = response.headers['x-secret-homework-header']
+
+        print(response.headers)
+        assert actual_val == expected_val, f"Cookie values are not equal"
